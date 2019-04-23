6d40d3bae587bd0b401c20e07aebc53eef82639c
Laura Webber <wwebby1@gmail.com>
2019-04-23 02:37:01 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(a, s)
+  a.push(s)
+end
+
+def using_unshift(a, s)
+  a.unshift(s)
+end
+
+def using_pop(a)
+  a.pop
+end
+
+def pop_with_args(a)
+  a.pop(2)
+end
+
+def using_shift(a)
+  a.shift
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
+
+def using_concat(a,b)
+  a.concat(b)
+end
+
+def using_insert(a,n)
+  a.insert(4, n)
+end
+
+def using_uniq(a)
+  a.uniq
+end
+
+def using_flatten(a)
+  a.flatten
+end
+
+def using_delete(a,s)
+  a.delete(s)
+end
+
+def using_delete_at(a,i)
+  a.delete_at(i)
+end
